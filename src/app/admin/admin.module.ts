75083dd0ae397e0e0743ef6280f9756975ab02e2
mesaikat17 <mesaikat17@gmail.com>
2022-07-22 02:59:30 +0530

    Admin Components Add

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    AddComponent,
+    UpdateComponent,
+    DeleteComponent
+  ],
   imports: [
     CommonModule
   ]
